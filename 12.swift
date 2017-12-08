fca146f31b8ea04bb38e0d54daec7de29faf3b22
miraclesumail <857247710@qq.com>
2017-12-08 10:28:36 +0800

    Create 12.swift

---
+++ 12.swift
@@ -0,0 +1,97 @@
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+        for _ in 0..<digitIndex {
+            decimalBase *= 10
+        }
+        return (self / decimalBase) % 10
+    }
+    
+    var add:Int {
+        return self + 3
+    }
+    
+    func mutilpy(_ num:Int) -> Int {
+        return self*num
+    }
+    
+    enum Kind {
+        case negative, zero, positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+print(746381295[2])
+print(3.add)
+print(5.mutilpy(6))
+print((-8).kind)
+
+func swapTwoInts( a: inout Int, b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(a:&someInt, b:&anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+
+protocol SomeProtocol {
+    static func someTypeMethod()
+}
+
+class test:SomeProtocol{
+    static func someTypeMethod(){
+        print("eee")
+    }
+    
+    
+}
+
+test().someTypeMethod()
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+// Prints "Here's a random number: 0.37464991998171"
+print("And another one: \(generator.random())")
+// Prints "And another one: 0.729023776863283"
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
